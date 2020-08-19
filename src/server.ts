7609fadea03ed0ff55afcf7b60a689cf5de22166
jairo quispe <joelquispeunju@gmail.com>
2020-08-18 21:03:24 -0300

    Add eslint & jest

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
-import express from "express";
+import express from 'express'
 
 const app = express()
 
 app.get('/', (req, res) => {
-    res.json({msg: "Hello world!"})
+  res.json({ msg: 'Hello world!' })
 })
 
 app.listen(3000, (err) => {
-    if(err) console.log(err);
-    console.log("Listening on port 3000");
+  if (err) console.log(err)
+  console.log('Listening on port 3000')
 })
